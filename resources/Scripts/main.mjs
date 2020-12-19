3cf6d084884edb3aec6b0400a0f6db04b6d28fba
Ronseplants <szerod@gmail.com>
2020-12-18 23:43:21 -0500

    maybe??

--- resources/Scripts/main.mjs
+++ resources/Scripts/main.mjs
@@ -4,11 +4,15 @@ const image = document.getElementById('me');
 
 let imgArray = [];
 
-const dir = await fs.promises.opendir('../images')
+async function search(path) {
+    const dir = await fs.promises.opendir(path)
 
-for await (const dirent of dir) {
-    imgArray.push(dirent.name);
+    for await (const dirent of dir) {
+        imgArray.push(dirent.name);
+    }
 }
+
+search('../images')
 
 image.addEventListener('click', () => {
     let newImage = imgArray[Math.floor(Math.random()*imgArray.length)]
